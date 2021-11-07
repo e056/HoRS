8d813eb539c22103c7bb2d8020fb4534e17552e4
angely <e0564603@u.nus.edu>
2021-11-07 16:35:35 +0800

    Updated Front Office Module

--- HoRSManagementClient/src/java/horsmanagementclient/FrontOfficeModule.java
+++ HoRSManagementClient/src/java/horsmanagementclient/FrontOfficeModule.java
@@ -5,10 +5,173 @@
  */
 package horsmanagementclient;
 
+import ejb.session.stateful.WalkInRoomReservationSessionBeanRemote;
+import ejb.session.stateless.RoomSessionBeanRemote;
+import entity.Employee;
+import entity.Reservation;
+import entity.Room;
+import entity.RoomReservationLineEntity;
+import java.math.BigDecimal;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import util.enumeration.AccessRightEnum;
+import util.exception.CreateNewReservationException;
+import util.exception.InvalidAccessRightException;
+import util.exception.RoomNotFoundException;
+import util.exception.RoomRateNotFoundException;
+
 /**
  *
  * @author ANGELY
  */
 public class FrontOfficeModule {
-    
+
+    private WalkInRoomReservationSessionBeanRemote walkInRoomReservationSessionBeanRemote;
+    private RoomSessionBeanRemote roomSessionBeanRemote;
+    private Employee currEmployee;
+
+    public FrontOfficeModule(WalkInRoomReservationSessionBeanRemote walkInRoomReservationSessionBeanRemote, RoomSessionBeanRemote roomSessionBeanRemote, Employee currEmployee) {
+        this.walkInRoomReservationSessionBeanRemote = walkInRoomReservationSessionBeanRemote;
+        this.roomSessionBeanRemote = roomSessionBeanRemote;
+        this.currEmployee = currEmployee;
+    }
+
+    public void menuFrontOffice() throws InvalidAccessRightException {
+        if (currEmployee.getAccessRightEnum() != AccessRightEnum.GUEST_RELATION) {
+            throw new InvalidAccessRightException("You don't have GUEST RELATION OFFICER rights to access the front office operation module.");
+        }
+        Scanner scanner = new Scanner(System.in);
+        Integer response = 0;
+
+        while (true) {
+            System.out.println("*** HoRS System :: Front Office ***\n");
+            System.out.println("1: Walk-in Search Room");
+            System.out.println("2: Check-In Guest");
+            System.out.println("3: Check-Out Guest");
+            System.out.println("4: Back\n");
+            response = 0;
+
+            while (response < 1 || response > 3) {
+                System.out.print("> ");
+
+                response = scanner.nextInt();
+
+                if (response == 1) {
+                    searchRoom();
+                } else if (response == 2) {
+                    //checkInGuest();
+                } else if (response == 3) {
+                    //checkOutGuest();
+                } else if (response == 4) {
+                    break;
+                } else {
+                    System.out.println("Invalid option, please try again!\n");
+                }
+            }
+
+            if (response == 4) {
+                break;
+            }
+        }
+    }
+
+    public void searchRoom() {
+        try {
+            Scanner scanner = new Scanner(System.in);
+            Integer response = 0;
+            SimpleDateFormat inputDateFormat = new SimpleDateFormat("d/M/y");
+            SimpleDateFormat outputDateFormat = new SimpleDateFormat("dd/MM/yyyy hh:mm a");
+            Date startDate;
+            Date endDate;
+            Room room;
+            String roomNumber;
+            String moreItem = "";
+            String comfirmReservation = "";
+
+            System.out.println("*** Hotel Reservation System :: Walk-In Search Room ***\n");
+            System.out.print("Enter Check-In Date (dd/mm/yyyy)> ");
+            startDate = inputDateFormat.parse(scanner.nextLine().trim());
+            System.out.print("Enter Check-Out Date (dd/mm/yyyy)> ");
+            endDate = inputDateFormat.parse(scanner.nextLine().trim());
+            System.out.println("------------------------");
+
+            List<Room> rooms = walkInRoomReservationSessionBeanRemote.walkInSearchRoom(startDate, endDate);
+            System.out.printf("%8s%20s%20s\n", "ID", "Room Number", "Room Type");
+            for (Room r : rooms) {
+                System.out.printf("%8s%20s%20s\n", r.getRoomId(), r.getRoomNumber(), r.getRoomType().getName());
+            }
+
+            System.out.println("------------------------");
+            System.out.println("1: Make Reservation");
+            System.out.println("2: Back\n");
+            System.out.print("> ");
+            response = scanner.nextInt();
+            scanner.nextLine();
+
+            if (response == 1) {
+                do {
+                    System.out.print("Enter Room Number> ");
+                    roomNumber = scanner.nextLine().trim();
+
+                    try {
+                        room = roomSessionBeanRemote.retrieveRoomByRoomNumber(roomNumber);
+                        System.out.println("Selecting Room = " + room.getRoomNumber());
+                        BigDecimal subTotal = walkInRoomReservationSessionBeanRemote.addRoom(room);
+                        System.out.println(room.getRoomNumber() + " added successfully!: "
+                                + "Price of stay for this room @ " + NumberFormat.getCurrencyInstance().format(subTotal) + "\n");
+                    } catch (RoomNotFoundException ex) {
+                        System.out.println("An error has occurred while retrieving room: " + ex.getMessage() + "\n");
+                    } catch (RoomRateNotFoundException ex) {
+                        System.out.println("An error has occurred while retrieving room: The room has no published rate");
+
+                    }
+
+                    System.out.print("More item? (Enter 'N' to complete reservation)> ");
+                    moreItem = scanner.nextLine().trim();
+                } while (!moreItem.equals("N"));
+
+                if (walkInRoomReservationSessionBeanRemote.getNumOfRooms() > 0) {
+                    System.out.println("Reserving the following rooms:\n");
+                    System.out.printf("\n%20s%30s", "Room Number", "Price of room for duration of stay");
+
+                    for (RoomReservationLineEntity roomReservationLineEntity : walkInRoomReservationSessionBeanRemote.getLineEntities()) {
+                        System.out.printf("\n%20s%30s",
+                                roomReservationLineEntity.getRoom().getRoomNumber(),
+                                NumberFormat.getCurrencyInstance().format(roomReservationLineEntity.getPrice()));
+                    }
+                    System.out.printf("\nNumber of Rooms: %d, Total Amount: %s\n",
+                            walkInRoomReservationSessionBeanRemote.getNumOfRooms(),
+                            NumberFormat.getCurrencyInstance().format(walkInRoomReservationSessionBeanRemote.getTotalAmount()));
+
+                    System.out.println("------------------------");
+                    System.out.print("Confirm reservation? (Enter 'Y' to complete reservation)> ");
+                    comfirmReservation = scanner.nextLine().trim();
+                    if (comfirmReservation.equals("Y")) {
+
+                        Reservation reservation = walkInRoomReservationSessionBeanRemote.walkInReserveRoom();
+                        System.out.println("Reservation completed successfully!: " + reservation.getReservationId() + "\n");
+                    } else {
+                        System.out.println("Cancelled.");
+                        walkInRoomReservationSessionBeanRemote.clear();
+                    }
+
+                } else {
+                    System.out.println("Nothing to reserve!");
+                    walkInRoomReservationSessionBeanRemote.clear();
+                }
+
+            }
+        } catch (ParseException ex) {
+            System.out.println("Invalid date input!\n");
+        } catch (CreateNewReservationException ex) {
+            Logger.getLogger(FrontOfficeModule.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
 }
