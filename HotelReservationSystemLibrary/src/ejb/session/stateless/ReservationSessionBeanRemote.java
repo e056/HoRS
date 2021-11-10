46afbee46db71d2245b62b8259a31b8675cf7c9f
angely <e0564603@u.nus.edu>
2021-11-10 12:25:54 +0800

    Updated checkin

--- HotelReservationSystemLibrary/src/ejb/session/stateless/ReservationSessionBeanRemote.java
+++ HotelReservationSystemLibrary/src/ejb/session/stateless/ReservationSessionBeanRemote.java
@@ -32,6 +32,4 @@ public interface ReservationSessionBeanRemote {
     public List<Reservation> retrieveCheckedInReservationByGuestId(Long guestId);
 
     public List<Reservation> retrieveCheckedInReservationByWalkInGuestId(Long guestId);
-
-    public List<Reservation> retrieveCheckedInReservationByWalkInGuestId(Long guestId);
 }
